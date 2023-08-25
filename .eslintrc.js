fa0752fee5830725dacb3c5acf257207c2a4e3d6
ashiqdey <ashiqdey@gmail.com>
2023-08-25 21:13:52 +0530

    added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// .eslintrc.js
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+        'prettier/prettier': 'error',
+    },
+    ignorePatterns: ['dist/'],
+};
