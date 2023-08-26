af83162d97107f033ca93c3b733623966bf7463b
ashiqdey <ashiqdey@gmail.com>
2023-08-26 19:25:38 +0530

    added supertest example for api testing

--- src/test/general.test.ts
+++ src/test/general.test.ts
@@ -1,6 +1,6 @@
-// import request from 'supertest';
+import request from 'supertest';
 import axios from 'axios';
-// import app from '../routes/general.route';
+import app from '../app'
 
 
 const url = 'http://127.0.0.1:5000';
@@ -15,3 +15,39 @@ describe('GET /', () => {
         expect(res.data).toMatch(/^Response from backend at/);
     },20000);
 });
+
+describe("POST /orm/validate", () => {
+    describe("validate body", () => {
+  
+      test("should respond status 400", async () => {
+        const response = await request(app).post("/orm/validate").send({
+            "email":"ashiqdey2@gmail.com",
+            "name":"ashique dey",
+            "mobile":"123456789", // invalid
+            "password":"qwerty@123"
+        })
+        expect(response.statusCode).toBe(400)
+      })
+      test("should specify json in the content type header", async () => {
+        const response = await request(app).post("/orm/validate").send({
+            "email":"ashiqdey2@gmail.com",
+            "name":"ashique dey",
+            "mobile":"1234567890",
+            "password":"qwerty@123"
+        })
+        expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+      })
+      test("response has key email", async () => {
+        const response = await request(app).post("/orm/validate").send({
+            "email":"ashiqdey2@gmail.com",
+            "name":"ashique dey",
+            "mobile":"1234567890",
+            "password":"qwerty@123"
+        })
+        expect(response.body.email).toBeDefined()
+      })
+    })
+  
+ 
+  
+  });
