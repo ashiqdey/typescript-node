6c394622d39375ba2d028b4f76c709f83f8132d1
ashiqdey <ashiqdey@gmail.com>
2023-08-26 19:02:36 +0530

    code refactored

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,8 @@
 import 'reflect-metadata';
-import express, { Application } from 'express';
 
+import app from './app'
 import { AppDataSource } from './data-source';
 
-import generalRoutes from './routes/general.route';
-import typesRoutes from './routes/types.route';
-import ormRoutes from './routes/orm.route';
-
-const app: Application = express();
-
-// -----------middleware for an Express.js-----------
-// parses incoming requests with JSON payloads
-app.use(express.json());
-// parses incoming requests with URL-encoded payloads
-app.use(express.urlencoded({ extended: true }));
-// -----------middleware for an Express.js-----------
-
-// --------routes---------
-app.use('/', generalRoutes);
-app.use('/types', typesRoutes);
-app.use('/orm', ormRoutes);
-// --------routes---------
 
 AppDataSource.initialize()
   .then(async () => {
